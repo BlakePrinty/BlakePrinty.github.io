cedc6915438f3a4afe01911477ca4f47a5f76d7d
Blake Printy <blakehprinty@gmail.com>
2022-08-24 17:56:43 -0400

    Added about me section

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ const darkModeButton = document.querySelector(".slider");
 const pageHeader = document.querySelector(".header");
 const toggleSwitchDiv = document.querySelector(".toggleSwitch");
 const pageFooter = document.querySelector(".footer");
+const downArrow = document.querySelector(".down-arrow");
+const aboutMe = document.querySelector(".about");
+const projects = document.querySelector(".projects");
+
+let darkMode = false;
 
 darkModeButton.addEventListener("click", () => {
     if (pageHeader.classList.contains("dark")) {
@@ -10,13 +15,22 @@ darkModeButton.addEventListener("click", () => {
         pageHeader.classList.remove("dark");
         toggleSwitchDiv.classList.remove("dark");
         pageFooter.classList.remove("dark");
+        downArrow.classList.remove("white");
+        aboutMe.classList.remove("dark");
+        projects.classList.remove("dark");
 
     } else {
 
         pageHeader.classList.add("dark");
         toggleSwitchDiv.classList.add("dark");
         pageFooter.classList.add("dark");
+        downArrow.classList.add("white");
+        aboutMe.classList.add("dark");
+        projects.classList.add("dark");
 
     }
 });
 
+function makeDarkMode() {
+    
+}

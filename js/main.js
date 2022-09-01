cd84202978eba2c3388aac617d67f4b321f5784a
Blake Printy <blakehprinty@gmail.com>
2022-08-31 20:39:31 -0400

    Added projects section

--- js/main.js
+++ js/main.js
@@ -3,17 +3,27 @@ const main = document.querySelector(".main");
 
 const downArrow = document.querySelector(".downArrow");
 const logo = document.querySelector(".logo");
+const cursor = document.querySelector(".cursor");
+// const projectImg = document.querySelectorAll(".img");
 
 logo.addEventListener("click", () => {
     
     if (main.classList.contains("dark")) {
+
         main.classList.remove("dark");
         logo.classList.add("inverted");
+        cursor.classList.add("inverted");
+        // projectImg.classList.remove("dark-border");
         downArrow.classList.remove("inverted");
+    
     } else {
+
         main.classList.add("dark");
         logo.classList.remove("inverted");
+        cursor.classList.remove("inverted");
+        // projectImg.classList.add("dark-border");
         downArrow.classList.add("inverted");
+    
     }
 
 });

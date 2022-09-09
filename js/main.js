a70485c11d1edd5d9b564f7124ec7873563dca6b
Blake Printy <blakehprinty@gmail.com>
2022-09-09 15:25:40 -0400

    New an improved portfolio

--- js/main.js
+++ js/main.js
@@ -1,29 +1,33 @@
+var tl = new TimelineMax({onUpdate:updatePercentage});
+var tl2 = new TimelineMax();
+const controller = new ScrollMagic.Controller();
 
-const main = document.querySelector(".main");
+tl.from('blockquote', .5, {x:200, opacity: 0});
+tl.from('span', 1, { width: 0}, "=-.5");
+tl.from('#office', 1, {x:-200, opacity: 0,ease: Power4.easeInOut}, "=-1");
+tl.from('#building', 1, {x:200, opacity: 0, ease: Power4.easeInOut}, "=-.7");
 
-const downArrow = document.querySelector(".downArrow");
-const logo = document.querySelector(".logo");
-const cursor = document.querySelector(".cursor");
-// const projectImg = document.querySelectorAll(".img");
+tl2.from("#box", 1, {opacity: 0, scale: 0});
+tl2.to("#box", .5, {left: "20%", scale: 1.3, borderColor: 'white', borderWidth: 12, boxShadow: '1px 1px 0px 0px rgba(0,0,0,0.09)'})
 
-logo.addEventListener("click", () => {
-    
-    if (main.classList.contains("dark")) {
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".sticky",
+            triggerHook: "onLeave",
+            duration: "100%"
+})
+  .setPin(".sticky")
+  .setTween(tl)
+		.addTo(controller);
 
-        main.classList.remove("dark");
-        logo.classList.add("inverted");
-        cursor.classList.add("inverted");
-        // projectImg.classList.remove("dark-border");
-        downArrow.classList.remove("inverted");
-    
-    } else {
+const scene2 = new ScrollMagic.Scene({
+  triggerElement: "blockquote"
+})
+  .setTween(tl2)
+		.addTo(controller);
 
-        main.classList.add("dark");
-        logo.classList.remove("inverted");
-        cursor.classList.remove("inverted");
-        // projectImg.classList.add("dark-border");
-        downArrow.classList.add("inverted");
-    
-    }
 
-});
+function updatePercentage() {
+  //percent.innerHTML = (tl.progress() *100 ).toFixed();
+  tl.progress();
+  console.log(tl.progress());
+}

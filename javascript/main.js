45954ce8e1dd494844f40aafdb66a69c339966b3
Blake Printy <blakehprinty@gmail.com>
2023-01-24 15:23:43 -0500

    Updated resume

--- javascript/main.js
+++ javascript/main.js
@@ -6,3 +6,24 @@ function scrollUpdater() {
     var scrolled = (winScroll / height) * 100;
     document.getElementById("scrollBar").style.width = scrolled + "%";
 }
+
+var background = document.querySelector(".background");
+var habiTrack = document.querySelector(".habiTrack");
+var albumSongs = document.querySelector(".albumSongs");
+var wordleSolver = document.querySelector(".wordleSolver");
+
+background.addEventListener("click", () => {
+    background.classList.add("hidden");    
+})
+
+habiTrack.addEventListener("click", () => {
+    background.classList.remove("hidden");
+})
+
+albumSongs.addEventListener("click", () => {
+    background.classList.remove("hidden");
+})
+
+wordleSolver.addEventListener("click", () => {
+    background.classList.remove("hidden");
+})
